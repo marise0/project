49ee392d765e777b50e5149a71672d1fffe297a0
Marise Nassar <marise00000@gmail.com>
2025-02-15 14:13:47 +0200

    added cases and control files

--- scripts/annovar.sh
+++ scripts/annovar.sh
@@ -4,8 +4,14 @@
 source <(python -m scripts.export)
 
 # Loop through all VCF files in the specified directory
-for vcf in "$VCF_PATH"/*.vcf; do
+for vcf in "$VCF_PATH"/cases/*.vcf; do
     echo "Processing VCF: $vcf"
     # Run the table_annovar.pl command for each VCF file
-   "$DOC"/annovar/table_annovar.pl "$vcf" "$DOC"/annovar/humandb/ -out "$OUTPUT_PATH"/$(basename "$vcf" .vcf) -vcfinput -buildver hg19 -protocol refGene -operation g -nastring . -remove
+   "$DOC"/annovar/table_annovar.pl "$vcf" "$DOC"/annovar/humandb/ -out "$OUTPUT_PATH"/cases/$(basename "$vcf" .vcf) -vcfinput -buildver hg19 -protocol refGene -operation g -nastring . -remove
+done
+
+for vcf in "$VCF_PATH"/control/*.vcf; do
+    echo "Processing VCF: $vcf"
+    # Run the table_annovar.pl command for each VCF file
+   "$DOC"/annovar/table_annovar.pl "$vcf" "$DOC"/annovar/humandb/ -out "$OUTPUT_PATH"/control/$(basename "$vcf" .vcf) -vcfinput -buildver hg19 -protocol refGene -operation g -nastring . -remove
 done
