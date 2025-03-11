3eb85c66556a66402539e258cce4b0e0a48cdadd
Marise Nassar <marise00000@gmail.com>
2025-03-11 17:04:07 +0200

    multi-sampleVCF--> multi sample vcfs

--- scripts/split.sh
+++ scripts/split.sh
@@ -3,11 +3,16 @@
 # Run Python script to export environment variables
 source <(python -m scripts.export)
 
-# Define VCF file path
-vcf="$VCF_PATH/CAGI_exome_hg19.gatk.snps.vcf"
+# Path to the input VCF file
+vcf_file="$VCF_PATH/CAGI_exome_hg19.gatk.snps.vcf"
 
-# Extract sample names and loop through them
-gatk --java-options "-Xmx4g" VariantsToTable \
-   -V data/CAGI_exome_hg19.gatk.snps.vcf \
-   -F SAMPLE \
-   -O sample_list.txt
+# Loop through the sample names
+for sample in $(bcftools query -l $vcf_file); do
+    if [[ $sample == CD* ]]; then
+        # If the sample starts with "CD", it's a case
+        bcftools view -s $sample $vcf_file -o "$VCF_PATH/cases/${sample}.vcf"
+    elif [[ $sample == H* ]]; then
+        # If the sample starts with "H", it's a control
+        bcftools view -s $sample $vcf_file -o "$VCF_PATH/control/${sample}.vcf"
+    fi
+done
