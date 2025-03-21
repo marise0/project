9b3cfb7c332a087a2f79d7e42bf40ebe2d036743
Marise Nassar <marise00000@gmail.com>
2025-03-21 10:27:50 +0200

    working on SNPs

--- scripts/split.sh
+++ scripts/split.sh
@@ -7,14 +7,21 @@ source <(python -m scripts.export)
 vcf_file=$VCF_PATH"/CAGI_exome_hg19.gatk.snps.vcf.gz"
 
 # Loop through the sample names
+rsids=("rs2066844" "rs2066845" "rs5743271" "rs41313262" "rs76418789" "rs104895444" "rs104895467" "rs34536443" "rs2476601" "rs11209026" "rs3764147")
+
+rsid_filter=$(printf 'ID=="%s" || ' "${rsids[@]}")
+rsid_filter=${rsid_filter% || }  # Remove trailing ' || '
+
 for sample in $(bcftools query -l $vcf_file); do
     if [[ $sample == CD* ]]; then
         # If the sample starts with "CD", it's a case
-        bcftools view -s $sample -r chr16:50700000-50800000 $vcf_file | \
-        bcftools filter -i 'FORMAT/GT!="0/0"' -Ov -o "$VCF_PATH/cases/${sample}.vcf"
+        bcftools view -s $sample $vcf_file | \
+        bcftools filter -i 'FORMAT/GT!="0/0"' | \
+        bcftools view -i "$rsid_filter" -Ov -o "$VCF_PATH/cases/${sample}.vcf"
     elif [[ $sample == H* ]]; then
         # If the sample starts with "H", it's a control
-        bcftools view -s $sample -r chr16:50700000-50800000 $vcf_file | \
-        bcftools filter -i 'FORMAT/GT!="0/0"' -Ov -o "$VCF_PATH/control/${sample}.vcf"
+        bcftools view -s $sample $vcf_file | \
+        bcftools filter -i 'FORMAT/GT!="0/0"' | \
+        bcftools view -i "$rsid_filter" -Ov -o "$VCF_PATH/control/${sample}.vcf"
     fi
 done
